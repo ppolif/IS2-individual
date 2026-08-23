d0166ac315cba3d5cd4eb8519a4d164d74fa457c
ppolif <paulina.ferrarisanchez@gmail.com>
2026-08-23 12:04:58 -0300

    guia audiovisual empezada

---
+++ TP1/ejercicio4/prueba-thymeleaf/src/main/java/com/example/prueba_thymeleaf/controller/SaludoController.java
@@ -0,0 +1,16 @@
+package com.example.prueba_thymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class SaludoController {
+
+    @GetMapping("/saludo")
+    public String saludo(@RequestParam String nombre, Model model) {
+        model.addAttribute("nombre", nombre);
+        return "saludo";
+    }
+}
---
+++ TP1/ejercicio4/prueba-thymeleaf/src/test/java/com/example/prueba_thymeleaf/PruebaThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prueba_thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
